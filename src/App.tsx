cc84619a696802af448876e4ed30664f2717b505
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-05-08 01:03:53 +0200

    Add routing and layout structure with new pages: About, Contact, OurStories, and YourStory; update dependencies in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
-function App() {
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import OurStories from "./pages/our-stories/OurStories";
+import YourStory from "./pages/your-story/YourStory";
 
-  return (
-    <div>Hello World</div>
-  )
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/our-stories",
+          element: <OurStories />,
+        },
+        {
+          path: "/your-story",
+          element: <YourStory />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
