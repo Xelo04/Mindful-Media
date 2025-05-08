346eecb3f3f80b1f8e114ca2f40d039cfa329635
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-05-08 17:38:07 +0200

    Deleted unused svg, add translate.svg, make the Navbar dynamic using data.ts

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,36 @@
 import "./navbar.scss";
+import { Link, useLocation } from "react-router-dom";
+import { navigationBar } from "../../data";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  const location = useLocation();
+
+  const getPageTitle = () => {
+    const currentPage = navigationBar.find(
+      (item) => item.url === location.pathname
+    );
+    return currentPage ? currentPage.title : "Couldn't find the page";
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <ul className="navbar-links">
+          {navigationBar.map((item) => (
+            <li key={item.id}>
+              <Link to={item.url}>
+                <span>{item.navigationTitle}</span>
+              </Link>
+            </li>
+          ))}
+          <li>
+            <img src="translate.svg" alt="Translate" />
+          </li>
+        </ul>
+      </nav>
+      <div className="page-title">{getPageTitle()}</div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/data.ts
@@ -0,0 +1,26 @@
+export const navigationBar = [
+  {
+    id: 1,
+    navigationTitle: "ABOUT US",
+    url: "/",
+    title: "Welcome to the About Page",
+  },
+  {
+    id: 2,
+    navigationTitle: "OUR STORIES",
+    url: "/our-stories",
+    title: "Explore Our Stories",
+  },
+  {
+    id: 3,
+    navigationTitle: "RESOURCES",
+    url: "/contact",
+    title: "Get in Touch with Us",
+  },
+  {
+    id: 4,
+    navigationTitle: "SHARE YOUR STORY",
+    url: "/your-story",
+    title: "Share Your Story",
+  },
+];
