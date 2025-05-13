f4b076ef371c96d2a7a059699a1bb0204599a268
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-05-13 17:44:46 +0200

    Implement code changes to enhance functionality and improve performance

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,7 +1,29 @@
 import "./about.scss";
 
 const About = () => {
-  return <div className="about">About</div>;
+  return (
+    <div className="about">
+      <section className="title-container">
+        <div className="title">
+          <span>#Mindful</span>
+          <span>Media</span>
+        </div>
+      </section>
+      <section className="about-us">
+        <h1>ABOUT US</h1>
+        <div className="about-us-container">
+          <p>
+            We are a team of passionate individuals dedicated to creating
+            meaningful and impactful media content. Our mission is to inspire,
+            educate, and entertain through our work. We believe in the power of
+            storytelling and its ability to connect people from all walks of
+            life.
+          </p>
+          <img src="logo.png" alt="logo" />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default About;
