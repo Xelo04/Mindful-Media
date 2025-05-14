3cf743dd9ccaf038c68eaa2a8eae9b7f6335736d
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-05-14 17:21:53 +0200

    Add yourStory page title

--- src/pages/your-story/YourStory.tsx
+++ src/pages/your-story/YourStory.tsx
@@ -1,7 +1,11 @@
-import "./yourstory.scss";
+import "./yourStory.scss";
 
 const YourStory = () => {
-  return <div className="yourstory">YourStory</div>;
+  return (
+    <div className="yourStory">
+      <h1>YourStory</h1>
+    </div>
+  );
 };
 
 export default YourStory;
