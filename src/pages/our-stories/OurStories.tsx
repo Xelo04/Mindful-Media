1fa34d4451769878fa0b393e05ee4b99e37e2046
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-05-14 17:08:55 +0200

    Refactor styles: update navbar and about page gradients, enhance h1 styling in global styles

--- src/pages/our-stories/OurStories.tsx
+++ src/pages/our-stories/OurStories.tsx
@@ -1,7 +1,12 @@
 import "./ourStories.scss";
 
 const OurStories = () => {
-  return <div className="ourStories">OurStories</div>;
+  return (
+    <div className="ourStories">
+      <h1>Our Stories</h1>
+      <span>no elo</span>
+    </div>
+  );
 };
 
 export default OurStories;
