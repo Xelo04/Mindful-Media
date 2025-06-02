f4974ed6bbecf8794d5d29407625e1f546ae80e6
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-06-02 22:31:31 +0200

    Implement responsive hamburger menu and navigation overlay for mobile view

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,20 +2,34 @@
 // It also uses a navbarData array from data.ts to generate the links in the navbar
 // navbarData = [{id, navigationTitle, url, pageTitle}]
 
+import { useState } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { navbarData } from "../../data";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav>
-      <ul>
+      <div className="navbar-header">
+        <button
+          className="hamburger"
+          onClick={() => setMenuOpen((open) => !open)}
+          aria-label={menuOpen ? "Close menu" : "Open menu"}
+        >
+          <span className="material-icons">{menuOpen ? "close" : "menu"}</span>
+        </button>
+      </div>
+      <ul className={`nav-links${menuOpen ? " open" : ""}`}>
         <div className="links">
           {navbarData.map((item) => (
-            <Link to={item.url}>
+            <Link
+              to={item.url}
+              key={item.id}
+              onClick={() => setMenuOpen(false)}
+            >
               <li
-                key={item.id}
-                // Add className to SHARE YOUR STORY link to match design
                 className={
                   item.navigationTitle === "SHARE YOUR STORY"
                     ? "your-story"
@@ -27,12 +41,21 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
-        <li>
-          <img src="translate.svg" alt="Translate" />
-        </li>
       </ul>
+      {/* Optional: overlay to close menu when clicking outside */}
+      {menuOpen && (
+        <div className="nav-overlay" onClick={() => setMenuOpen(false)} />
+      )}
     </nav>
   );
 };
 
 export default Navbar;
+{
+  /* Translate image */
+}
+{
+  /* <li>
+          <img src="translate.svg" alt="Translate" />
+        </li> */
+}
